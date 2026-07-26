7217db1a1437a47268e632349c1d5f5ca3e27b19
Omkar Ture <tureomkar12@gmail.com>
2026-07-26 10:21:28 +0530

    Solved Two Sum problem

---
+++ DSA-Seriess/src/Arrays/Traversal.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+public class Traversal {
+    public static void main(String[] args) {
+        int[] arr = {10,30,20,50,40};
+
+//        for(int i=0; i<arr.length; i++){
+//            System.out.print(arr[i]+" ");
+//        }
+
+        // by using for each loop
+
+          for(int no:arr){
+              System.out.print(no+" ");
+          }
+    }
+}
+
+
+
+
+
